31ebf2c41b411cf1ccde13286c2201d3c6b3ebb8
Shaun Thompson <shaunthompson@fullsail.edu>
2013-02-13 20:34:37 -0500

    Completed clear and view storage options!

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
 // Wait until the DOM is ready
 window.addEventListener("DOMContentLoaded", function(){
 
+	//Toggle Controls
+	function toggleControls(n){
+		switch(n){
+			case "on":
+				$('form').style.display = "none";
+				$('clear').style.display = "inline";
+				$('displayLink').style.display = "none";
+				$('addNew').style.display = "inline";
+				break;
+			case "off":
+				$('form').style.display = "block";
+				$('clear').style.display = "inline";
+				$('displayLink').style.display = "inline";
+				$('addNew').style.display = "none";	
+				$('items').style.display = "none";			
+				break;
+			default:
+				return false;
+		};
+	};
+
 	//Save data into local storage
-/*	function storeData(key){
-	//find value of radio
-		getSelectedRadio();
-	//save form elements into storage
-		localStorage.setItem("Date", $('date').value);
-		localStorage.setItem("Type", $('type').value);	
-		localStorage.setItem("Group", $('group').value);
-		localStorage.setItem("Name", $('name').value);
-		localStorage.setItem("Calories", $('calories').value);
-		localStorage.setItem("Notes", $('notes').value);
-		
-		alert("Meal Saved!");
-	}; */
-	
 	function storeData(){
 		var id				= Math.floor(Math.random()*1000000001);
 		//Gather up all our form field values and store them in an object
 		//Object properties contain array with form label and input values
+		getSelectedRadio();
 		var item			= {};
 			item.date		= ["Date: ", $('date').value];
 			item.type		= ["Meal Type: ", $('type').value];
@@ -29,11 +37,20 @@ window.addEventListener("DOMContentLoaded", function(){
 			item.notes		= ["Additional Notes: ", $('notes').value];
 		//Save data to local storage
 			localStorage.setItem(id, JSON.stringify(item));
-			alert("Contacted Saved!");
+			alert("Meal Saved!");
 			
 	};
 	
-	
+	//find value of selected radio button
+	function getSelectedRadio(){
+		var radio = document.forms[0].group;
+		for(var i=0; i<radio.length; i++){
+			if(radio[i].checked){
+				groupValue = radio[i].value;
+			};
+		};
+	};
+		
 	//clear all data
 	function clearLocal(){
 		if(localStorage.length === 0){
@@ -70,99 +87,54 @@ window.addEventListener("DOMContentLoaded", function(){
 		selectLi.appendChild(makeSelect);
 	};
 
+	//Variable Defaults
+	var mealType = ["---Choose A Meal Type---", "Breakfast", "Lunch", "Dinner", "Other"],
+		groupValue;
 
-	// Variable Defaults
-	var mealType = ["---Choose A Meal Type---", "Breakfast", "Lunch", "Dinner", "Other"];
+	//Write data from local storage to browser
+	function getData(){
+		if(localStorage.length === 0){
+			alert("There are no meals currently tracked.");
+		};
+		toggleControls("on");
+		var makeDiv = document.createElement('div');
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement('ul');
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$('items').style.display = "block";
+		for(var i=0, len=localStorage.length; i<len;i++){
+			var makeli = document.createElement('li');
+			makeList.appendChild(makeli);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			
+			//Convert the string from local storage
+			var obj = JSON.parse(value);
+			var makeSublist = document.createElement('ul');
+			makeli.appendChild(makeSublist);
+			for(var n in obj){
+				var makeSubli = document.createElement('li');
+				makeSublist.appendChild(makeSubli);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubli.innerHTML = optSubText;
+			};
+		};
+	};
+		
 	makeCats();
 
-/*
 
 	// Set link & Submit Click Events
 	var displayLink = $('displayLink');
 	displayLink.addEventListener("click", getData);
+	
 	var clearLink = $('clear');
 	clearLink.addEventListener("click", clearLocal);
 	
-*/
+
 	var save = $('submit');
 	save.addEventListener("click", storeData);
 
 
 });
-
-
-
-
-
-
-
-/*
-
-// Declare Variables
-var fname = document.getElementById("fname");
-
-// Capture & Retrieve Data
-var captureData = function(){
-	localStorage.setItem("First Name", fname.value);
-};
-
-var getData = function(){
-	var fnameKey = localStorage.key("First Name");
-	var fnameValue = localStorage.getItem(fnameKey);
-	fname.value = fnameValue;
-};
-
-
-// Event Listeners
-
-
-
-
-var checkboxes = document.etElementByID("myForm").meal;
-console.log(checkboxes);
-
-var getValue = function(){
-	for(i=0, j=checkboxes.length; i<j; i++){
-		if(checkboxes[i].checked){
-			console.log(checkboxes[i].value);	
-		};
-	};
-};
-
-
-// JS
-
-Common JS Events
-
-click
-submit
-focus
-blur
-change
-mouseover
-mouseout
-
-
-
-var myField = document.getElementByID("myText");
-console.log(myField);
-
-var changeBorder = function(){
-	textField.setAttribute("class", hasFocus);
-};
-
-var defaultBorder = function(){
-	textField.removeAttribute("class", hasFocus);
-};
-
-var myList = document.createElement("ul");
-console.log(myList);
-
-search.addEventListener("click", changeBorder);
-textField.addEventListener("focus", changeBorder);
-textField.addEventListener("blur", defaultBorder);
-
-
-var tags = document.getElementsByTagName("li");
-console.log(tags);
-*/
